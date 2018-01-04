09c70d7287787e3e049ba14f593618434489f8ee
Jim Kyle <jim@jwkyle.com>
2018-01-03 23:48:42 -0600

    add alias for nimble, parse green

--- lib/ledgernizer.ex
+++ lib/ledgernizer.ex
@@ -1,5 +1,11 @@
 defmodule Ledgernizer do
+  alias NimbleCSV.RFC4180, as: CSV
   def list_transactions do
     File.read!("lib/statement.csv")
+    |> parse
+  end
+
+  defp parse(string) do #defp = private function
+    CSV.parse_string(string)
   end
 end
