f24bc1b4af3002a61dc65b4703c033e3c6fcee4e
Jim Kyle <jim@jwkyle.com>
2018-01-03 23:03:25 -0600

    remove gen txt

--- lib/ledgernizer.ex
+++ lib/ledgernizer.ex
@@ -1,17 +1,4 @@
 defmodule Ledgernizer do
-  @moduledoc """
-  Documentation for Ledgernizer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ledgernizer.hello
-      :world
-
-  """
   def hello do
     :world
   end
