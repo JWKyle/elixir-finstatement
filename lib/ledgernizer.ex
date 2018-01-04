cfd29c1e3d82baad9db06de08390b1edbc9027c5
Jim Kyle <jim@jwkyle.com>
2018-01-03 23:53:41 -0600

    add filter function

--- lib/ledgernizer.ex
+++ lib/ledgernizer.ex
@@ -3,9 +3,14 @@ defmodule Ledgernizer do
   def list_transactions do
     File.read!("lib/statement.csv")
     |> parse
+    |> filter
   end
 
   defp parse(string) do #defp = private function
     CSV.parse_string(string)
+  end
+
+  defp filter(rows) do
+    Enum.map(rows, fn(row) -> Enum.drop(row, 1)end)
   end
 end
